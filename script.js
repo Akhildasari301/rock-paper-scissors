977d55c0dd08d184d435d4c6138055bc47e67aa5
Akhil <dasariakhil2001@gmail.com>
2022-06-02 12:24:21 +0530

    Add UI to the game

--- script.js
+++ script.js
@@ -9,37 +9,105 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    game_count += 1;
     if (playerSelection == computerSelection) {
-        return "That's a tie!!";
+        round_winner.textContent = "Tie game"
+        tieScore += 1;
+        tie_score_div.textContent = tieScore;
     } else if (playerSelection == "rock" && computerSelection == "scissors"){
-        return "You win, rock beats scissors";
+        pScore += 1
+        player_score_div.textContent = pScore;
+        round_winner.textContent = "Player Won"
     } else if (playerSelection == "scissors" && computerSelection == "paper"){
-        return "You win, scissors beats paper!";
+        // console.log("chose scissor");
+        round_winner.textContent = "Player Won"
+        pScore += 1
+        player_score_div.textContent = pScore;
     } else if (playerSelection == "paper" && computerSelection == "rock"){
-        return "You win, paper beats rock";
+        round_winner.textContent = "Player Won"
+        pScore += 1
+        player_score_div.textContent = pScore;
     }
-    else if (computerSelection == "rock" && computerSelection == "scissors"){
-        return "You Lose, rock beats scissors";
+    else if (computerSelection == "rock" && playerSelection == "scissors"){
+        round_winner.textContent = "Computer Won"
+        cScore += 1
+        computer_score_div.textContent = cScore;
     } else if (computerSelection == "scissors" && playerSelection == "paper"){
-        return "You Lose, scissors beats paper!";
+        round_winner.textContent = "Computer Won"
+        cScore += 1
+        computer_score_div.textContent = cScore;
     } else if (computerSelection == "paper" && playerSelection == "rock"){
-        return "You Lose, paper beats rock";
-    } else {
-        return "Wrong input!!";
+        round_winner.textContent = "Computer Won"
+        cScore += 1
+        computer_score_div.textContent = cScore;
+    } 
+    if (game_count == 5){
+        gameOver.textContent = "Game Over!!"
+        if (pScore > cScore){
+            winner.textContent = "Player won the game. Refresh page to play again!";
+        } else if (pScore < cScore){
+            winner.textContent = "Computer won the game. Refresh page to play again!";
+        } else {
+            winner.textContent = "It's a tie. Refresh page to play again!"
+        }
+        return
     }
 }
 
-function play() {
-    for (let i = 0; i < 5; i++){
-        playerSelection = prompt("Enter your choice: ");
-        computerSelection = computerPlay()
-        console.log("Your choice: ", playerSelection);
-        console.log("computer's choice: ", computerSelection);
-        result = playRound(playerSelection, computerSelection);
-        message = `Round ${i+1}: ${result}`;
-        console.log(message);
-    }
-}
+// function play() {
+//     for (let i = 0; i < 5; i++){
+//         playerSelection = prompt("Enter your choice: ");
+//         computerSelection = computerPlay()
+//         console.log("Your choice: ", playerSelection);
+//         console.log("computer's choice: ", computerSelection);
+//         result = playRound(playerSelection, computerSelection);
+//         message = `Round ${i+1}: ${result}`;
+//         console.log(message);
+//     }
+// }
+let game_count = 0;
+let rock = document.querySelector('.rock')
+let paper = document.querySelector('.paper')
+let scissors = document.querySelector('.scissors')
+let player_score_div = document.querySelector('.player-score')
+let computer_score_div = document.querySelector('.computer-score')
+let tie_score_div = document.querySelector('.tie-score')
+let pScore = cScore = tieScore = 0;
+let round_winner = document.querySelector('.round-winner');
+let winner = document.querySelector('.winner');
+let gameOver = document.querySelector('.game-over');
+player_score_div.textContent = 0;
+computer_score_div.textContent = 0;
+tie_score_div.textContent = 0;
 
-play()
 
+
+// rock.addEventListener('click', () => playRound('rock', computerPlay()));
+// paper.addEventListener('click', () => playRound('paper', computerPlay()))
+// scissors.addEventListener('click', () => playRound('scissors', computerPlay()))
+
+
+rock.addEventListener('click', () => {
+    playRound('rock', computerPlay());
+    console.log("you chose rock");
+    console.log(`pScore: ${pScore}`)
+    console.log(`cScore: ${cScore}`)
+    console.log(`tieScore: ${tieScore}`)
+});
+
+paper.addEventListener('click', () => {
+    playRound('paper', computerPlay());
+    console.log("you chose paper");
+    console.log(`pScore: ${pScore}`)
+    console.log(`cScore: ${cScore}`)
+    console.log(`tieScore: ${tieScore}`)
+});
+
+scissors.addEventListener('click', () => {
+    playRound('scissors', computerPlay());
+    console.log("you chose scissors");
+    console.log(`pScore: ${pScore}`)
+    console.log(`cScore: ${cScore}`)
+    console.log(`tieScore: ${tieScore}`)
+
+});
